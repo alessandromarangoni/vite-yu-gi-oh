61069ba1668e5bab3287533a944c80a39417bab2
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-25 11:39:56 +0200

    cambio on select "mancano tutte le cards al loading"

--- vite-yu-gi-oh/src/appMain.vue
+++ vite-yu-gi-oh/src/appMain.vue
@@ -1,4 +1,5 @@
 <script>
+import { vShow } from 'vue';
 import { store } from './store/store';
 import axios from 'axios';
 
@@ -7,29 +8,49 @@ export default {
     data() {
         return {
             store,
+            tipoScelto: ''
         }
     },
     methods: {
         getCardImageUrl() {
             axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
-                .then(r => { store.carte.push(r.data.data) })
+                .then(r => {
+                    store.carte.push(r.data.data)
+                    this.getCardImageArchetipo()
+                })
                 .catch(e => console.log(e))
-
+        },
+        getCardImageArchetipo() {
+            for (let i = 0; i < store.carte[0].length; i++) {
+                const archetipo = store.carte[0][i].archetype;
+                if (archetipo !== undefined && (!store.archetipi.includes(archetipo))) {
+                    this.store.archetipi.push(archetipo)
+                }
+            }
+            console.log(store.archetipi)
+        },
+        changeSelect(valore) {
+            this.tipoScelto = valore
+            console.log(this.tipoScelto)
         }
     },
-    beforeMount() {
+    mounted() {
         this.getCardImageUrl()
     },
-    mounted() {
-        console.log(store)
-    }
 }
 </script>
 
 <template>
     <div class="container">
+        <div>
+            <select name="archetype" v-model="this.tipoScelto" id="">
+                <option v-for="archetipo in store.archetipi" @change="changeSelect(archetipo)" :value="archetipo" selected>
+                    {{ archetipo }}
+                </option>
+            </select>
+        </div>
         <div class="cards_container">
-            <div class="" v-for="(carta, i) in store.carte[0]">
+            <div class="" v-for="(carta, i) in store.carte[0]" v-show="carta.archetype == this.tipoScelto">
                 <img :src="carta.card_images[0].image_url" alt="">
                 <h4>{{ carta.name }}</h4>
                 <p>{{ carta.archetype }}</p>
